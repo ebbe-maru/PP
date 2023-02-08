0f1baa980a3d3f3e25c087e023102f13c253571a
ebbe-maru <ebb-e@hotmail.dk>
2023-02-08 09:23:47 +0100

    2-csharp

--- Excercises/Hello World/main.cs
+++ Excercises/Hello World/main.cs
@@ -2,7 +2,7 @@ class main{
 static int Main(){
 	System.Console.Write("hello\n user ");
 	System.Console.WriteLine("hello world ");
-	System.Console.Write("hello\n user ");
+	System.Console.Write("hello user ");
 	return 0;
 	
 }
