2378bb901d0fe70f1a87b2a98556a35d7ba91c86
ebbe-maru <ebb-e@hotmail.dk>
2023-02-08 09:28:26 +0100

    csharp_2

---
+++ Excercises/hello_2/main.cs
@@ -0,0 +1,9 @@
+using System;
+using static System.Console;
+using static System.Math;
+class main {
+    public static void Main(){
+        static_hello.print();
+        static_world.print();
+    }
+}
