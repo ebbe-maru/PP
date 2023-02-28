b3c817eecf7fe4eccce22d0ab4cd1dd1f69fbb4f
ebbe-maru <ebb-e@hotmail.dk>
2023-02-28 12:57:01 +0100

    Vector

--- Excercises/Epsilon Vec/Vector/main.cs
+++ Excercises/Epsilon Vec/Vector/main.cs
@@ -4,6 +4,19 @@ using static System.Math;
 
 public static class main{
     public static void Main() {
+       vec u =  new vec();
+       u.print("u = ");
 
+       vec v = new vec(1, 2, 3);
+       v.print("v = ");
+
+       vec u1 = 2 * v;
+       u1.print("u1 = 2*v");
+
+       vec v1 = v-u1;
+       v1.print("v1 = v - u1 = ");
+
+       double dotp = v.dot(v1);
+       WriteLine($"v.dot(v1) = {dotp}");
     }
 }
